331bae10d8084fe305e2d0b29b573f1011dd507d
Son Nguyen Ngoc <sonnn.se@gmail.com>
2017-05-10 01:49:45 +0800

    2nd commit

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ const publicPath = express.static(path.join(__dirname, './public/dist'))
 app.use('/assets/', publicPath);
 app.get('/', (req, res) => { res.sendFile(indexPath) });
 
-app.listen(process.env.PORT || '80', () => {
-  console.log('Example app listening on port 80!')
+app.listen(process.env.PORT || '3000', () => {
+  console.log('Example app listening on port 3000!')
 });
